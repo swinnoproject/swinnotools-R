9252d372edafb9c05ea21edd459504e58b377909
Philipp Jonas Kreutzer <philipp_jonas.kreutzer@ekh.lu.se>
2023-04-06 10:37:21 +0200

    add filter_in_columns function
    helps to filter data spread across different columns such as use sectors of product codes

--- R/filter_usesectors.R
+++ R/filter_usesectors.R
@@ -1,28 +1,37 @@
-#' This function concatenates columns that start with a certain string and filters rows based on a list of values.
+#' Filter Dataframe for Unique IDs Based on Filtered Values in Specific Columns
 #'
-#' @param df A data frame.
-#' @param old_col_start A character string representing the start of the column names that need to be concatenated.
-#' @param new_col_name A character string representing the name of the new column that stores the concatenated values.
-#' @param position An integer representing the position where the new column should be inserted.
-#' @param values A list of values used to filter the rows of the data frame.
+#' This function takes in a dataframe, a range of columns to filter on, a specified ID column,
+#' and a list of filter values. The function first converts the specified range of columns to character
+#' if necessary, then melts the dataframe into a long format based on the range of columns, and
+#' finally filters for unique IDs if the "use_sectors" column contains a value specified in the
+#' filter_values list. This function requires the dplyr package to run.
 #'
-#' @return A data frame with concatenated columns and filtered rows.
+#' @param df The input dataframe to filter
+#' @param use_cols A character vector indicating the range of columns to filter on
+#' @param id_col A character string indicating the name of the ID column to filter on
+#' @param filter_values A character vector containing the filter values to search for in the "use_sectors" column
+#'
+#' @return A vector of unique IDs that have values in the specified filter_values in the specified range of columns
+#'
+#' @import dplyr
 #' @export
 #'
 #' @examples
-#' swinno <- data.frame(use_sector_1 = c("7", "8", "9"), use_sector_2 = c("7", "8", "9"), id = c("1", "2", "3"))
-#' filter_sectors(swinno, "use_sector", "use_sectors", 2, c("6", "7"))
-#'
-#' @importFrom base apply
-#' @importFrom base cbind
-#' @importFrom base grepl
-#'
-filter_sectors <- function(df, old_col_start, new_col_name, position, values) {
-  temp_df <- df[, grepl(old_col_start, names(df))] # subset columns that start with old_col_start
-  temp_col <- apply(temp_df, 1, paste0, collapse = "") # concatenate columns row-wise
-  temp_df <- cbind(temp_col, df[, -which(names(df) %in% names(temp_df))]) # add concatenated column to original df
+#' swinno
+#' use_sectors_of_interest = c("123", "45")
+#' filter_in_columns(swinno, use_cols=c("use_sectors", "use_sector_1", "use_sector_2"), "sinno_id", filter_values = use_sectors_of_interest)
+filter_in_columns <- function(df, use_cols, id_col, filter_values) {
+  df <- df[, c(id_col, use_cols)]
+  # Convert columns starting with "use_" to character if necessary
+  use_cols_to_char <- sapply(df[, use_cols], is.numeric)
+  df[, use_cols[use_cols_to_char]] <- lapply(df[, use_cols[use_cols_to_char]], as.character)
 
-  filtered_df <- temp_df[temp_df[, new_col_name] %in% values, ] # subset rows based on values in the new_col_name column
+  # Melt dataframe into long format based on the range of columns
+  df_long <- dplyr::pivot_longer(df, cols=use_cols, names_to=NULL, values_to="use_sectors", values_drop_na = TRUE)
 
-  return(filtered_df)
+  # Filter for unique ids if the "use_sectors" column contains a value specified in a list
+  df_filtered <- subset(df_long, use_sectors %in% filter_values)
+  df_filtered <- unique(df_filtered[, id_col])
+
+  return(df_filtered)
 }
